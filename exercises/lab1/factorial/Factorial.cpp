4fd404a3cd7bcbe118d4d7480b5e55db8958741c
WojciechDolata <33723173+WojciechDolata@users.noreply.github.com>
2018-03-06 15:20:39 +0100

    Add files via upload
    zrobiony factorial

--- exercises/lab1/factorial/Factorial.cpp
+++ exercises/lab1/factorial/Factorial.cpp
@@ -4,6 +4,13 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-
+    if (value<=-13 or value>=13)
+        return 0;
+    else if (value == 0)
+        return 1;
+    else if (value>0)
+        return factorial(value-1)*value;
+    else
+        return factorial(value+1)*value;
   return 0;
 }
