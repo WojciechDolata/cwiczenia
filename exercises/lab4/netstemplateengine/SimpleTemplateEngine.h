a6dafc9b5beb576eecb74a32dc42926b908a3424
WojciechDolata <wojciech.k.dolata@gmail.com>
2018-04-03 10:09:24 +0200

    simple net engine działa, bez testu injection

--- exercises/lab4/netstemplateengine/SimpleTemplateEngine.h
+++ exercises/lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -5,10 +5,20 @@
 #ifndef JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
 #define JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
 
+#include <string>
+#include <unordered_map>
+#include <regex>
 
-class SimpleTemplateEngine {
+namespace nets {
+    class View {
+    public:
+        View(std::string);
+        std::string Render(const std::unordered_map <std::string, std::string> &model) const;
 
-};
+    private:
+        std::string _text;
+    };
+}
 
 
 #endif //JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
--- exercises/lab4/netstemplateengine/main.cpp
+++ exercises/lab4/netstemplateengine/main.cpp
@@ -2,3 +2,10 @@
 // Created by dolawojc on 3/30/18.
 //
 
+#include <stdlib.h>
+#include <iostream>
+#include "SimpleTemplateEngine.h"
+
+int main(){
+    return 0;
+}
